b740296f529145c1386b58a089775d9a1773bffd
Kevin Nguyen <ndh.it219@gmail.com>
2016-05-16 03:28:25 +0700

    Init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+arr = ['HTML5', 'CSS3', 'JavaScript', 'JQuery', 'AngularJS',
+  'PHP', 'MySQL', 'CodeIgniter'];
+console.log(arr);
+
+/*
+  1. AngularJS - Single Page Application
+  2. NodeJS - Realtime Application
+  3. Sencha Touch - Mobile Application
+  4. ExtJS - Web Manager Application
+  5. jQuery
+*/
