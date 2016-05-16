fd335a84d13ca6ebbafb4d2d90d1ea0d10155ad0
Kevin Nguyen <ndh.it219@gmail.com>
2016-05-17 00:52:07 +0700

    mdf 2

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-arr = ['HTML5', 'CSS3', 'JavaScript', 'JQuery', 'AngularJS',
-  'PHP', 'MySQL', 'CodeIgniter'];
-console.log(arr);
+function func2() {
+  var t = document.getElementById('getText');
+  document.getElementById('showtext').innerHTML = t.value;
+}
 
+function func() {
+  var element = document.querySelectorAll('div input.website');
+  document.getElementById('result').innerHTML = element[0].value;
+}
 /*
   1. AngularJS - Single Page Application
   2. NodeJS - Realtime Application
   3. Sencha Touch - Mobile Application
   4. ExtJS - Web Manager Application
   5. jQuery
-  
+  6. Electron
+
 */
