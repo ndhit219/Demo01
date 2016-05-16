c51736e3cd82067ea68fa8ca9a0d40ef039f2820
Kevin Nguyen <ndh.it219@gmail.com>
2016-05-17 04:45:37 +0700

    mdf 3

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-function func2() {
-  var t = document.getElementById('getText');
-  document.getElementById('showtext').innerHTML = t.value;
-}
+var node = document.createElement('p');
+var content = document.createTextNode("DOM Nodes in JavaScript");
+node.appendChild(content);
+node.style.color = 'green';
+node.style.textAlign = 'center';
+document.getElementsByTagName('body')[0].appendChild(node);
+var w = null;
+node.addEventListener('click', function () {
+  w = window.open('http://genk.vn','Window Child','width=400px,height=400px')
+  return false;
+})
 
-function func() {
-  var element = document.querySelectorAll('div input.website');
-  document.getElementById('result').innerHTML = element[0].value;
-}
 /*
+getElement: id, tag, css all
+Event: 3c (addEventListener)
+DOM Node: add, insert, remove
+BOM: window, screen, window.location, history,
+  navigator, popup, timing, cookies
+------------------------------------------------
   1. AngularJS - Single Page Application
   2. NodeJS - Realtime Application
   3. Sencha Touch - Mobile Application
